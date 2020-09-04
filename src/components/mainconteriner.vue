c28b4bbe028fcd9363a86e0e3e0f00a4cb10c3a0
Dyego Alves <dyego.santos.alves@gmail.com>
2020-09-04 12:17:52 -0400

    Content AdSense Aceept

--- src/components/mainconteriner.vue
+++ src/components/mainconteriner.vue
@@ -1,8 +1,23 @@
 <template>
-  <main role="main" class="inner cover">
+  <main role="main" class="inner cover box-sizing">
     <h1 class="cover-heading">ConvertON</h1>
     <p class="lead">Currency conversion app, measurements, and others</p>
     <p class="lead">In development</p>
+
+    <h3>Why convert currencies online?</h3>
+    <p>
+      It was not long ago that people visited exchange offices to check the price of international currencies. Which demanded a series of costs with transportation, logistics, in addition to time.
+      With the advancement of the internet, however, it became possible to make this conversion online. Without leaving home or anywhere in the world.
+      The conversion platforms on the internet brought practicality to the process. In just a few clicks, you have access to different currency quotes.
+      With the FinanceOne converter, for example, you have at your disposal an online platform with a secure database, passed on by the Central Bank. That is, it combines security material in a fast and virtual process.
+    </p>
+
+    <h3>Do I pay any tax to convert?</h3>
+    <p>
+      Many people seek to convert currencies to complete a purchase abroad. In view of this, it is common for the question to arise: “Do I pay any tax to carry out the conversion?”
+      Yes, to convert currencies in Wolrd, the user has to pay the Tax on Financial Operations.
+    </p>
+
     <p class="lead">
       <a href="#" class="btn btn-sm btn-secondary">Learn more</a>
     </p>
